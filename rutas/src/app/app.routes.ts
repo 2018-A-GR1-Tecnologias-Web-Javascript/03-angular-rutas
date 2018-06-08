f3ae5b8169e6afe645882e4486ae7127f7b34e13
Adrian Eguez <eadepto@hotmail.com>
2018-06-08 16:24:57 -0500

    commit inicial

---
+++ rutas/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {Routes} from "@angular/router";
+import {RutaInicioComponent} from "./ruta-inicio/ruta-inicio.component";
+
+const RUTAS_APP: Routes = [
+  {
+    path: 'inicio',
+    component: RutaInicioComponent
+  },
+  {
+    path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  {path: '**', component: PageNotFoundComponent}
+];
